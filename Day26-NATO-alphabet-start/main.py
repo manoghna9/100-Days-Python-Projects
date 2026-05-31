7f5a470bfba76279a10a065fb31b289ccba33a41
Sumashri Kodandaramaiah <suma@Sumas-MacBook-Air.local>
2026-05-31 09:24:53 +0530

    added exceptions

--- Day26-NATO-alphabet-start/main.py
+++ Day26-NATO-alphabet-start/main.py
@@ -1,18 +1,19 @@
-import pandas as pd
+import pandas 
 
-# reads csv file
-data = pd.read_csv("/Users/suma/code/Gitdemo/100-Days-PythonProj/Day26-NATO-alphabet-start/nato_phonetic_alphabet.csv")
+data = pandas.read_csv("/Users/suma/code/Gitdemo/100-Days-PythonProj/Day26-NATO-alphabet-start/nato_phonetic_alphabet.csv")
 
-# creates dictionary from dataframe
 phonetic_dict = {
     row.letter: row.code
     for (index, row) in data.iterrows()
 }
 
-# asks user for a word
 word = input("Enter a word: ").upper()
 
-# converts each letter into nato code
-output_list = [phonetic_dict[letter] for letter in word]
+try:
+    output_list = [phonetic_dict[letter] for letter in word]
 
-print(output_list)
+except KeyError:
+    print("Sorry, only letters in the alphabet please.")
+
+else:
+    print(output_list)
