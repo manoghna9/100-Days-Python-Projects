d927def1046fded95f10ccd85f6340df3f889c06
Sumashri Kodandaramaiah <suma@Sumas-MacBook-Air.local>
2026-07-14 07:47:51 +0530

    blog with css

---
+++ Day57-BlogJinja/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__) #creates flask application instance
+
+
+@app.route("/")
+def home():
+    response = requests.get(
+    "https://api.npoint.io/c790b4d5cab58020d391",
+    timeout=5)    
+    all_posts = response.json()
+
+    return render_template(
+        "index.html",
+        posts=all_posts
+    )
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5006)
